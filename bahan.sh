7813e21d699568a5e37f746cfc4f4e2b69a6c9e2
B41B4L <48058322+B41B4L@users.noreply.github.com>
2019-05-11 05:07:49 +0700

    bahan.sh

--- bahan.sh
+++ bahan.sh
@@ -1,7 +1,9 @@
 pkg install figlet
 pkg install Ruby
+pkg install lolcat
 gem install lolcat
 pkg install curl
 pip install termcolor
 clear
 figlet -f slant Suscses | lolcat
+echo "Selanjutnya Ketik sh dav.sh"
